e433c360619a09a3e9ce20a60e89469b8bee37a3
Vincent Cabansag <vcabansag@gmail.com>
2013-08-22 14:37:30 -0500

    Added model associations.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ActiveRecord::Base
+
+  has_many :users, through: :invites
+  has_many :invites
+  belongs_to :host, class_name: 'User'
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+
+  has_many :invites
+  has_many :events, through: :invites
+
 end
